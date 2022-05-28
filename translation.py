73bb0578ae80713b69742afb2d260d8d2f720625
Kubinic <106214811+Kubinic@users.noreply.github.com>
2022-05-28 19:12:44 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,7 +2,7 @@ rus_eng = {
     'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть',
     'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'
 }
-
+ 
 def num_translate(eng_word):
     return rus_eng.get(eng_word)
-print(num_translate('two'))
+print(num_translate('two')) 
